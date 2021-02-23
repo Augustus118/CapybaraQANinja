8fa589c8a73e32ce46ef1d54ee698a10ec8fe683
Agostinho Bernardes <aabernardes@gmail.com>
2021-02-23 19:35:34 -0300

    Aula Upload arquivos

---
+++ ruby/capy/spec/upload_spec.rb
@@ -0,0 +1,43 @@
+
+describe 'upload', :upload do
+
+
+    before(:each) do
+      
+        visit 'https://training-wheels-protocol.herokuapp.com/upload'
+        @arquivo = Dir.pwd + '/spec/fixtures/Teste.txt'
+        @image = Dir.pwd + '/spec/fixtures/Fluxo.png'
+
+    end
+
+
+    it 'Upload com arquivo texto' do
+
+        attach_file('file-upload', @arquivo)
+        click_button 'Upload'
+
+        div_arquivo = find('#uploaded-file')
+        expect(div_arquivo.text).to eql 'Teste.txt'
+
+    end
+
+    it 'Upload com imagem' do
+
+        attach_file('file-upload', @image)
+        click_button 'Upload'
+
+        sleep 5
+        img = find('#new-image')
+        expect(img[:src]).to include '/uploads/Fluxo.png'
+
+    end
+
+
+
+    after(:each) do
+        sleep 3
+
+    end
+
+
+end    
