4323d40a1a404725b4f182650906d9f249bed092
Agostinho Bernardes <aabernardes@gmail.com>
2021-03-24 09:30:50 -0300

    spec key - envio arrays teclas

---
+++ ruby/capy/spec/key_spec.rb
@@ -0,0 +1,48 @@
+describe 'Teclado', :key do
+
+    before (:each) do
+        visit 'https://training-wheels-protocol.herokuapp.com/key_presses'
+
+    end
+
+
+    it 'Simulando as teclas simbolos' do
+
+        find('#campo-id').send_keys :space
+        expect(page).to have_content 'You entered: SPACE'
+
+        sleep 3
+
+
+    end
+    
+    
+    it 'Array Simbolos' do
+
+        teclas = %i[tab escape space enter shift control alt]
+
+        teclas.each do |t|
+            find('#campo-id').send_keys t
+            expect(page).to have_content 'You entered: ' + t.to_s.upcase
+            sleep 2
+
+        end
+
+    end
+    
+    
+    it 'Array Letras' do
+
+        teclas = %w[a n g e l a]
+
+        teclas.each do |l|
+            find('#campo-id').send_keys l
+            expect(page).to have_content 'You entered: ' + l.to_s.upcase
+            sleep 2
+
+        end
+
+    end 
+
+
+end    
